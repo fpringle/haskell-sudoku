9eed308a7199088edbb6606b1dfa7d5e964d2bc7
fpringle <freddyjepringle@gmail.com>
2022-10-26 12:42:58 +0200

    License and copyright

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,11 @@
+{-
+Copyright (c) 2022, Frederick Pringle
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+-}
+
 module Main where
 
 import Network.Wai.Handler.Warp (run)
--- testsuite/Sudoku/Tests/Defs.hs
+++ testsuite/Sudoku/Tests/Defs.hs
@@ -1,3 +1,11 @@
+{-
+Copyright (c) 2022, Frederick Pringle
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+-}
+
 module Sudoku.Tests.Defs where
 
 import Test.QuickCheck
