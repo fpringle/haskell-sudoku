4d63c749ce0a234bb85a7b432a77dc7c1f3c2424
fpringle <freddyjepringle@gmail.com>
2022-10-26 12:13:31 +0200

    tidy up JS test runner

--- testsuite/test-server-js/index.js
+++ testsuite/test-server-js/index.js
@@ -1,6 +1,6 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 
-const sugoku_url = 'https://sugoku.herokuapp.com/'
+const sugoku_url = 'https://sugoku2.herokuapp.com/'
 const my_url = 'http://localhost:3421/'
 
 const encodeBoard = (board) => board.reduce((result, row, i) => result + `%5B${encodeURIComponent(row)}%5D${i === board.length -1 ? '' : '%2C'}`, '')
@@ -22,35 +22,52 @@ function get_puzzle(difficulty) {
 }
 
 function get_sugoku_solution(board) {
+  const start_time = Date.now();
   const sol_url = sugoku_url + 'solve';
+
   return fetch(sol_url, {
     method: 'POST',
     body: encodeParams(board),
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-  }).then(response => response.json()).then(response => response.solution).catch(console.warn);
+  }).then(response => {
+    const time_taken = Date.now() - start_time;
+    return response.json().then(json => [json.solution, time_taken]);
+  }).catch(console.warn);
 }
 
 function get_my_solution(board) {
+  const start_time = Date.now();
   const my_sol_url = my_url + 'solve';
   return fetch(my_sol_url, {
     method: 'POST',
     body: JSON.stringify(board),
-  }).then(response => response.json()).then(response => response.board).catch(console.warn);
+  }).then(response => {
+    const time_taken = Date.now() - start_time;
+    return response.json().then(json => [json.board, time_taken]);
+  }).catch(console.warn);
 }
+
+console.log('Fetching a "hard" sudoku puzzle from sugoku2.herokuapp.com');
 
 get_puzzle('hard').then(board => {
   console.log('Puzzle:');
   print_board(board.board);
+  console.log('\nGetting solutions\n');
+
   return Promise.all([
     get_sugoku_solution(board),
     get_my_solution(board),
   ]);
-}).then(([sugoku_sol, my_sol]) => {
-  console.log('Sugoku solution:');
+}).then(([[sugoku_sol, sugoku_time], [my_sol, my_time]]) => {
+  console.log(`Solution from sugoku2.herokuapp.com (${sugoku_time} ms):`);
   print_board(sugoku_sol);
-  console.log('My solution:');
+
+  console.log(`\nMy solution (${my_time} ms):`);
   print_board(my_sol);
+  console.log();
+
   const ss = sol => sol.map(row => row.join('')).join('');
-  if (ss(sugoku_sol) === ss(my_sol)) console.log('same solution!');
-  else console.log('different solution...');
+
+  if (ss(sugoku_sol) === ss(my_sol)) console.log('Same solution!');
+  else console.log('Different solution...');
 });
